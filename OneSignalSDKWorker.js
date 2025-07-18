ac1a2ffa997c60af4001743e4ecb3255516fd2a0
mdyahhya <142885726+mdyahhya@users.noreply.github.com>
2025-07-18 23:58:49 +0530

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,4 @@
+// OneSignal Service Worker
+// This file must be served from your domain's root directory (e.g., yoursite.com/OneSignalSDKWorker.js)
+
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
